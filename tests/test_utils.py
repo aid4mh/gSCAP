b502829f055743c5bc2c5c6c72463b29bcde350e
lukeWaninger <luke.waninger@gmail.com>
2019-01-16 19:06:15 -0800

    .

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -58,12 +58,12 @@ class TestGPS(TestCase):
         pass
 
     def test_zip_from_dd_returns_neg_when_no_results(self):
-        results = utils.zip_from_dd(self.lat, self.lon, maxd=1)
+        results = utils.zip_from_dd(22, 122, maxd=1)
         self.assertTrue(results == -1)
 
     def test_zip_from_dd_returns_correct(self):
         results = utils.zip_from_dd(self.lat, self.lon)
-        self.assertTrue(results == 4631)
+        self.assertTrue(results == 47579)
 
     def test_isint(self):
         self.assertTrue(all(
