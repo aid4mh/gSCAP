9c222349cb5bf6456ef30e7a1e5a463a2b0a9f23
lukeWaninger <luke.waninger@gmail.com>
2019-01-21 06:27:01 -0800

    .

--- tests/test_gps.py
+++ tests/test_gps.py
@@ -1,7 +1,14 @@
 import datetime as dt
+import os
+from sqlite3 import dbapi2 as sqlite
 from unittest import skip, TestCase
 
+import numpy as np
 import pandas as pd
+import responses
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from gscapl import gps
 
@@ -13,15 +20,36 @@ class TestGPS(TestCase):
     @classmethod
     def setUpClass(cls):
         """perform at test class initialization
-
         """
-        pass
+        now = dt.datetime(
+            year=2005, month=5, day=5, hour=12
+        )
+        cls.day = dt.datetime(year=now.year, month=now.month, day=now.day)
+        cls.time = now.time()
+        cls.lat = 32.3788
+        cls.lon = -84.90685
+        cls.zipcode = 31905
+
+        cls.gcname = 'sqlite+pysqlite:///test_gps_cache.sqlite'
+        cls.del_cache()
+
+        engine = create_engine(cls.gcname, module=sqlite)
+        gps.Base.metadata.create_all(engine)
+        cls.session = sessionmaker(bind=engine)()
+
+        cls.cache_kwargs = {'engine': cls.gcname}
+
+    @classmethod
+    def del_cache(cls):
+        fn = cls.gcname.replace('sqlite+pysqlite:///', '')
+        if os.path.exists(fn):
+            os.remove(fn)
 
     @classmethod
     def tearDownClass(cls):
         """perform when all tests are complete
         """
-        pass
+        cls.del_cache()
 
     def setUp(self):
         """perform before each unittest"""
@@ -30,7 +58,10 @@ class TestGPS(TestCase):
     def tearDown(self):
         """perform after each unittest
         """
-        pass
+        t = self.session.query(gps.PlaceRequest).all()
+        for ti in t:
+            self.session.delete(ti)
+        self.session.commit()
 
     def test_gpsrecords_named_tuple_conversion(self):
         d = dt.datetime(year=2019, month=1, day=1)
